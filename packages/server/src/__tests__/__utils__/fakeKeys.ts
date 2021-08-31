84d90c972bed82c290a90520a840304ab005bf8a
rtang03 <7686266+rtang03@users.noreply.github.com>
2021-08-31 10:08:32 +0800

    add nonce and state check

---
+++ packages/server/src/__tests__/__utils__/fakeKeys.ts
@@ -0,0 +1,17 @@
+export const fakedKeys = {
+  publicKeyHex: 'e4d386e77cd7859b120f51082fb20cf02a446ddad9c47fb4eaf1ab38f05cb82c',
+  privateKeyHex:
+    '3080c66769bf0b7334f6fae79b0fd1fd96f29490c9c4b4123d339ea2cba57564e4d386e77cd7859b120f51082fb20cf02a446ddad9c47fb4eaf1ab38f05cb82c',
+  jwks: {
+    keys: [
+      {
+        kty: 'OKP',
+        crv: 'Ed25519',
+        alg: 'EdDSA',
+        kid: 'e4d386e77cd7859b120f51082fb20cf02a446ddad9c47fb4eaf1ab38f05cb82c',
+        x: '5NOG53zXhZsSD1EIL7IM8CpEbdrZxH-06vGrOPBcuCw',
+        d: 'MIDGZ2m_C3M09vrnmw_R_ZbylJDJxLQSPTOeosuldWTk04bnfNeFmxIPUQgvsgzwKkRt2tnEf7Tq8as48Fy4LA',
+      },
+    ],
+  },
+};
